4f830a484aee073d5898ddf4587a26dc66fac959
hendisantika <hendisantika@yahoo.co.id>
2024-11-14 06:03:09 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/crudcustomervaadin/CrudCustomerVaadinApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.crudcustomervaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCustomerVaadinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudCustomerVaadinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/crudcustomervaadin/CrudCustomerVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.crudcustomervaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCustomerVaadinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
