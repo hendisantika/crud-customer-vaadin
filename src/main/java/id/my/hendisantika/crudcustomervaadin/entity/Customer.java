976ba50b954828f68290dfe8c6ed6036eef098bc
hendisantika <hendisantika@yahoo.co.id>
2024-11-14 06:07:43 +0700

    Add Customer Entity🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/crudcustomervaadin/entity/Customer.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.crudcustomervaadin.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : crud-customer-vaadin
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/11/24
+ * Time: 06.07
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+}
