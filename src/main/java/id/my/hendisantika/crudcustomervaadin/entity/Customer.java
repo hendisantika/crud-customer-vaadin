c655309cbdb41f552f6c94d37cb68c9567723502
hendisantika <hendisantika@yahoo.co.id>
2024-11-14 06:23:33 +0700

    Ignore some files n folders🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/crudcustomervaadin/entity/Customer.java
+++ src/main/java/id/my/hendisantika/crudcustomervaadin/entity/Customer.java
@@ -1,5 +1,8 @@
 package id.my.hendisantika.crudcustomervaadin.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
