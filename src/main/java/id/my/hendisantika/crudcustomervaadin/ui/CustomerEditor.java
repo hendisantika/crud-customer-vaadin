f951a77152d6853e4495b27edee5a41fa1b2825e
hendisantika <hendisantika@yahoo.co.id>
2024-11-14 06:15:53 +0700

    Add CustomerEditor🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/crudcustomervaadin/ui/CustomerEditor.java
+++ src/main/java/id/my/hendisantika/crudcustomervaadin/ui/CustomerEditor.java
@@ -11,7 +11,9 @@ package id.my.hendisantika.crudcustomervaadin.ui;
  * To change this template use File | Settings | File Templates.
  */
 
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.KeyNotifier;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -19,6 +21,7 @@ import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import id.my.hendisantika.crudcustomervaadin.entity.Customer;
 import id.my.hendisantika.crudcustomervaadin.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * A simple example to introduce building forms. As your real application is probably much
@@ -47,4 +50,28 @@ public class CustomerEditor extends VerticalLayout implements KeyNotifier {
      */
     private Customer customer;
     private ChangeHandler changeHandler;
+
+    @Autowired
+    public CustomerEditor(CustomerRepository repository) {
+        this.repository = repository;
+
+        add(firstName, lastName, actions);
+
+        // bind using naming convention
+        binder.bindInstanceFields(this);
+
+        // Configure and style components
+        setSpacing(true);
+
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+
+        addKeyPressListener(Key.ENTER, e -> save());
+
+        // wire action buttons to save, delete and reset
+        save.addClickListener(e -> save());
+        delete.addClickListener(e -> delete());
+        cancel.addClickListener(e -> editCustomer(customer));
+        setVisible(false);
+    }
 }
