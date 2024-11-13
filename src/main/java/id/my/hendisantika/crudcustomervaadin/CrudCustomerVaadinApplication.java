b4cdbfd68ec5c8653a655505508f18783643f5ae
hendisantika <hendisantika@yahoo.co.id>
2024-11-14 06:19:18 +0700

    Init Data🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/crudcustomervaadin/CrudCustomerVaadinApplication.java
+++ src/main/java/id/my/hendisantika/crudcustomervaadin/CrudCustomerVaadinApplication.java
@@ -1,8 +1,14 @@
 package id.my.hendisantika.crudcustomervaadin;
 
+import id.my.hendisantika.crudcustomervaadin.entity.Customer;
+import id.my.hendisantika.crudcustomervaadin.repository.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class CrudCustomerVaadinApplication {
 
@@ -10,4 +16,39 @@ public class CrudCustomerVaadinApplication {
         SpringApplication.run(CrudCustomerVaadinApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner loadData(CustomerRepository repository) {
+        return (args) -> {
+            // save a couple of customers
+            repository.save(new Customer("Jack", "Reacher"));
+            repository.save(new Customer("Itadori", "Yuji"));
+            repository.save(new Customer("Kim", "Kardasian"));
+            repository.save(new Customer("David", "Beckam"));
+            repository.save(new Customer("Michelle", "Yeoh"));
+
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (Customer customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+            // fetch an individual customer by ID
+            Customer customer = repository.findById(1L).get();
+            log.info("Customer found with findOne(1L):");
+            log.info("--------------------------------");
+            log.info(customer.toString());
+            log.info("");
+
+            // fetch customers by last name
+            log.info("Customer found with findByLastNameStartsWithIgnoreCase('Bauer'):");
+            log.info("--------------------------------------------");
+            for (Customer bauer : repository
+                    .findByLastNameStartsWithIgnoreCase("Bauer")) {
+                log.info(bauer.toString());
+            }
+            log.info("");
+        };
+    }
 }
