9692ae6389b2673ac35c1ab77e9cbbf690f61a26
hendisantika <hendisantika@yahoo.co.id>
2024-11-14 07:05:05 +0700

    Add shouldFillOutComponentsWithDataWhenTheApplicationIsStarted🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/crudcustomervaadin/CrudCustomerVaadinApplicationTests.java
+++ src/test/java/id/my/hendisantika/crudcustomervaadin/CrudCustomerVaadinApplicationTests.java
@@ -1,13 +1,21 @@
 package id.my.hendisantika.crudcustomervaadin;
 
+import id.my.hendisantika.crudcustomervaadin.repository.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.BDDAssertions.then;
+
+@SpringBootTest(classes = CrudCustomerVaadinApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class CrudCustomerVaadinApplicationTests {
 
+    @Autowired
+    private CustomerRepository repository;
+
     @Test
-    void contextLoads() {
+    public void shouldFillOutComponentsWithDataWhenTheApplicationIsStarted() {
+        then(this.repository.count()).isEqualTo(5);
     }
 
 }
