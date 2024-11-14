15ee1fad60c303c4e49a9517abdf1ef167158247
hendisantika <hendisantika@yahoo.co.id>
2024-11-14 07:06:55 +0700

    Add shouldStoreCustomerInRepoWhenEditorSaveClicked🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/crudcustomervaadin/ui/CustomerEditorTest.java
+++ src/test/java/id/my/hendisantika/crudcustomervaadin/ui/CustomerEditorTest.java
@@ -2,9 +2,13 @@ package id.my.hendisantika.crudcustomervaadin.ui;
 
 import id.my.hendisantika.crudcustomervaadin.repository.CustomerRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.BDDMockito.then;
 
 /**
  * Created by IntelliJ IDEA.
@@ -34,4 +38,15 @@ public class CustomerEditorTests {
         editor.setChangeHandler(changeHandler);
     }
 
+    @Test
+    public void shouldStoreCustomerInRepoWhenEditorSaveClicked() {
+        emptyCustomerWasSetToForm();
+
+        this.editor.firstName.setValue(FIRST_NAME);
+        this.editor.lastName.setValue(LAST_NAME);
+
+        this.editor.save();
+
+        then(this.customerRepository).should().save(argThat(customerMatchesEditorFields()));
+    }
 }
